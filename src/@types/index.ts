984b2c7bd863330ee77778427fd9ae8826ef22b4
isimmons <isimmons33@gmail.com>
2023-10-05 12:49:44 -0400

    no truncate hover types

---
+++ src/@types/index.ts
@@ -0,0 +1,78 @@
+export type User = {
+  avatar: string;
+  email: string;
+  lastVisitAt: number;
+  name: string;
+  isModerator?: boolean;
+  registeredAt: number;
+  username: string;
+  usernameLower: string;
+  id: string;
+  bio?: string;
+  twitter?: string;
+  website?: string;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  slug: string;
+  forums: Array<string>;
+};
+
+export type Forum = {
+  categoryId: string;
+  description: string;
+  name: string;
+  slug: string;
+  id: string;
+  lastPostId?: string;
+  threads?: Array<string>;
+};
+
+export type Stats = {
+  postsCount: number;
+  threadsCount: number;
+  usersCount: number;
+  usersOnline: number;
+};
+
+export type Thread = {
+  firstPostId: string;
+  forumId: string;
+  lastPostAt: number;
+  lastPostId: string;
+  posts: Array<string>;
+  publishedAt: number;
+  slug: string;
+  title: string;
+  userId: string;
+  id: string;
+  contributors?: Array<string>;
+};
+
+export type ReactionEmoji = '❤️' | '👎' | '👍' | '💩' | '😄';
+export type ReactionIds = Record<string, string>;
+
+export type Post = {
+  edited?: {
+    at: number;
+    by: string;
+    moderated: boolean;
+  };
+  reactions?: Record<ReactionEmoji, ReactionIds>;
+  id: string;
+  userId: string;
+  threadId: string;
+  publishedAt: number;
+  text: string;
+};
+
+export type Data = {
+  users: Array<User>;
+  categories: Array<Category>;
+  forums: Array<Forum>;
+  stats: Stats;
+  threads: Array<Thread>;
+  posts: Array<Post>;
+};
